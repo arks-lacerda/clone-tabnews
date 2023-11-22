f6b2a0f6606fb5649ed8c53324bc3fbbd2f6e7a1
Arthur Lacerda <39713179+arks-lacerda@users.noreply.github.com>
2023-11-22 05:56:37 +0000

    adding a paragraph

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
  function Home() {
-    return <h1>My battles will be rewarded and my God is always present in my struggles!!</h1>
+    return (
+      <>
+         <h1>My battles will be rewarded and my God is always present in my struggles!!</h1>
+         <p>You will be unbeatable, always with God and Jesus.</p>
+      </>
+    )
  }
 
  export default Home;  
