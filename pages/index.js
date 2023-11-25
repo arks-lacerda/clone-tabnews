f9c51beafff271acc4d0f86f260a0010b8275820
Arthur Lacerda <39713179+arks-lacerda@users.noreply.github.com>
2023-11-25 05:37:27 +0000

    File modifications index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
- function Home() {
-    return (
-      <>
-         <h1>My battles will be rewarded and my God is always present in my struggles!!</h1>
-         <p>You will be unbeatable, always with God and Jesus.</p>
-      </>
-    )
- }
+function Home() {
+  return (
+    <>
+      <h1>
+        My battles will be rewarded and my God is always present in my
+        struggles!!
+      </h1>
+      <p>You will be unbeatable, always with God and Jesus.</p>
+    </>
+  );
+}
 
- export default Home;  
+export default Home;
