99e7a0c8e2414592a4e8afa7c9c67a523805ca0c
Arthur Lacerda <39713179+arks-lacerda@users.noreply.github.com>
2023-11-25 07:40:07 +0000

    add scripts and

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
   return (
-    <> 
+    <>
       <h1>
         My battles will be rewarded and my God is always present in my
         struggles!
@@ -8,6 +8,14 @@ function Home() {
       <p>You will be unbeatable, always with God and Jesus.</p>
     </>
   );
+}
+
+function test() {
+  console.log("test");
+}
+
+function autoSave() {
+  console.log("Auto Save disabled");
 }
 
 export default Home;
