ed4db1b7fe3c1b41c9d2aab8f9065331b283dad3
Arthur Lacerda <39713179+arks-lacerda@users.noreply.github.com>
2024-01-01 19:31:10 +0000

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,16 +1,33 @@
 import database from "infra/database.js"
+import { CONFIG_FILES } from "next/dist/shared/lib/constants";
 
 async function status(request, response) {
-    
-    const result = await database.query("SELECT 1 + 1 as sum;")
-    console.log(result.rows);
+    const updatedAt = new Date().toISOString();
 
-    response.status(200).json(
-        {
-            "student": "são acima da média",
-            "curso": "curso.dev"
+    const databaseVersionResult = await database.query("SHOW server_version;")
+    const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+    
+    const databaseMaxConnectionsResult = await database.query("SHOW max_connections;")
+    const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections
+
+    const databaseName = process.env.POSTGRES_DB;
+    const databaseOpenedConnectionsResult = await database.query({
+        text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1", 
+        values: [databaseName]
+    })
+    const databaseOpenedConnectionsValue = await databaseOpenedConnectionsResult.rows[0].count
+
+    response.status(200).json({
+        updated_at: updatedAt,
+        dependencies: {
+            database: {
+                version: databaseVersionValue,
+                max_connections: parseInt(databaseMaxConnectionsValue),
+                opened_connections: databaseOpenedConnectionsValue,
+            }
         }
-    );
+    });
+
 };
 
 export default status; 
